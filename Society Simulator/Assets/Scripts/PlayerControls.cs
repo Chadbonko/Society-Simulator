5649ca26c8ff9840d4bdde72ea48162af43b8a45
Bruce Lee <bruce.l640@stu.nebo.edu>
2019-12-06 10:17:09 -0700

    dadw

--- Society Simulator/Assets/Scripts/PlayerControls.cs
+++ Society Simulator/Assets/Scripts/PlayerControls.cs
@@ -7,11 +7,17 @@ public class PlayerControls : MonoBehaviour
     public float Speed = 10f;
     public float horizontaInput;
     public float forwardInput;
+    private Rigidbody playerRb;
+    private float gravityModifier = 1.5f;
+    public float jumpForce;
+    public float turnSpeed;
+    public bool isOnGround;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
@@ -20,10 +26,27 @@ public class PlayerControls : MonoBehaviour
         horizontaInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         //We will move the player forward
-        transform.Translate(Vector3.forward * Time.deltaTime * Speed * forwardInput);
+        transform.Translate(Vector3.back * Time.deltaTime * Speed * forwardInput);
         //we'll move the player side to side
-        transform.Translate(Vector3.right * Time.deltaTime * Speed * horizontaInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * Speed * horizontaInput * turnSpeed);
 
-
+        if (Input.GetKey(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce);
+            isOnGround = false;
+        }
     }
+    private void OnCollisionEnter(Collision collision)
+    { 
+        if (CompareTag ("Ground"))
+        {
+
+            isOnGround = true;
+
+        }
+      
+    }
+
+
+
 }
